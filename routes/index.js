93574f328f859a3ef0bdc757c460cac653575791
gywlsp <gywls517@gmail.com>
2022-07-06 16:05:31 +0900

    chore: :pencil2: 페이지 제목 수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My배민' });
 });
 
 module.exports = router;
