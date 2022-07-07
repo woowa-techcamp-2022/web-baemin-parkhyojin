7bccfdbc022f4ac0a0a2b77b27e6fc2bfd946c2c
gywlsp <gywls517@gmail.com>
2022-07-07 20:55:37 +0900

    feat: :sparkles: 회원가입 기능 추가

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const db = require('../lib/db');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', async (req, res) => {
+  db.get('users').push(req.body).write();
+  res.redirect('/signin');
 });
 
 module.exports = router;
