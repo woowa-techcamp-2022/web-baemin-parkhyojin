7f27018e3d61326668fe1107d875fa298d2aedd3
gywlsp <gywls517@gmail.com>
2022-07-07 09:01:42 +0900

    feat: :sparkles: 전체 동의 업데이트 함수 추가

--- public/javascripts/signup/agree.js
+++ public/javascripts/signup/agree.js
@@ -4,3 +4,11 @@ const checkAll = (checked) => {
         $el.checked = checked
     })
 }
+
+const updateAllChecked = () => {
+    const allChecked = [...document.querySelectorAll('input[type="checkbox"]')].reduce((acc, $el) => {
+        if($el.id==="all-agree" || $el.checked) return acc;
+        return false;
+    }, true);
+    document.querySelector('#all-agree').checked = allChecked;
+}
