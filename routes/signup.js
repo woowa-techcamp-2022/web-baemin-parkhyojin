d351d4ab1f64e20d45866874ed2baf8bbdb199f1
gywlsp <gywls517@gmail.com>
2022-07-07 20:04:31 +0900

    feat: :sparkles: signup detail 페이지 UI 추가

--- routes/signup.js
+++ routes/signup.js
@@ -9,4 +9,8 @@ router.get('/phone', function(req, res, next) {
   res.render('signup/phone', { title: '회원가입' });
 });
 
+router.get('/detail', function(req, res, next) {
+  res.render('signup/detail', { title: '회원가입' });
+});
+
 module.exports = router;
