efecca0cc676b0c1ede09f509274d8f786f2c7da
gywlsp <gywls517@gmail.com>
2022-07-08 09:40:18 +0900

    refactor: :recycle: signin 페이지 컴포넌트 이용해 리팩토링

--- routes/signin.js
+++ routes/signin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('signin');
+  res.render('signin/index');
 });
 
 module.exports = router;
