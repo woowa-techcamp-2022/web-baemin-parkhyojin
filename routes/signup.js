0617e8db00e18cd90d5e8e63ebcc2cc848a7ac0a
gywlsp <gywls517@gmail.com>
2022-07-08 10:39:25 +0900

    chore: :fire: 불필요한 title 데이터 삭제

--- routes/signup.js
+++ routes/signup.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/agree', function(req, res, next) {
-  res.render('signup/agree/index', { title: '회원가입' });
+  res.render('signup/agree/index');
 });
 
 router.get('/phone', function(req, res, next) {
-  res.render('signup/phone', { title: '회원가입' });
+  res.render('signup/phone');
 });
 
 router.get('/detail', function(req, res, next) {
-  res.render('signup/detail', { title: '회원가입' });
+  res.render('signup/detail');
 });
 
 module.exports = router;
